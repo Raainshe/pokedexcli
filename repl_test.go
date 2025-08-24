33d866143a3ec88513a938a7aeb1eef237c5974e
Raain <ryanbwgt@gmail.com>
2025-08-24 17:59:48 +0200

    added test functions for cache data

--- repl_test.go
+++ repl_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"strconv"
 	"testing"
+	"time"
+
+	pokecache "github.com/raainshe/pokedexcli/internal"
 )
 
 func TestCleanInput(t *testing.T) {
@@ -43,5 +46,59 @@ func TestCleanInput(t *testing.T) {
 				t.Fail()
 			}
 		}
+	}
+}
+
+func TestAddGet(t *testing.T) {
+	const interval = 5 * time.Second
+	cases := []struct {
+		key string
+		val []byte
+	}{
+		{
+			key: "https://pokeapi.co/api/v2/location-area?offset=0&limit=20",
+			val: []byte("testdata"),
+		},
+		{
+			key: "https://pokeapi.co/api/v2/location-area?offset=20&limit=20",
+			val: []byte("moretestdata"),
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
+			cache := pokecache.NewCache(interval)
+			cache.Add(c.key, c.val)
+			val, ok := cache.Get(c.key)
+			if !ok {
+				t.Errorf("expected to find key")
+				return
+			}
+			if string(val) != string(c.val) {
+				t.Errorf("expected to find value")
+				return
+			}
+		})
+	}
+}
+
+func TestReapLoop(t *testing.T) {
+	const baseTime = 5 * time.Millisecond
+	const waitTime = baseTime + 5*time.Second
+	cache := pokecache.NewCache(baseTime)
+	cache.Add("https://pokeapi.co/api/v2/location-area?offset=0&limit=20", []byte("testdata"))
+
+	_, ok := cache.Get("https://pokeapi.co/api/v2/location-area?offset=0&limit=20")
+	if !ok {
+		t.Errorf("expected to find key")
+		return
+	}
+
+	time.Sleep(waitTime)
+
+	_, ok = cache.Get("https://pokeapi.co/api/v2/location-area?offset=0&limit=20")
+	if ok {
+		t.Errorf("expected to not find key")
+		return
 	}
 }
